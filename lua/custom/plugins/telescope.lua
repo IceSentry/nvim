6747d119021e32c29b5d7b8f0eed8528dd87d23c
IceSentry <c.giguere42@gmail.com>
2024-10-14 17:56:37 -0400

    add todo telescope

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -101,5 +101,10 @@ return {
     vim.keymap.set('n', '<leader>sn', function()
       builtin.find_files { cwd = vim.fn.stdpath 'config' }
     end, { desc = '[S]earch [N]eovim files' })
+
+    -- Shortcut for searching your Neovim configuration files
+    vim.keymap.set('n', '<leader>st', function()
+      vim.cmd 'TodoTelescope'
+    end, { desc = '[S]earch [T]odo' })
   end,
 }
