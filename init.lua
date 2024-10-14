5d5cdfa3bacadab2d1adf94c65650d47f390a827
IceSentry <c.giguere42@gmail.com>
2024-10-13 23:40:16 -0400

    don't print in init.lua

--- init.lua
+++ init.lua
@@ -1,10 +1,8 @@
 if vim.g.vscode then
   -- VSCode extension
-  print 'Hello from vscode'
   require 'custom.core.keymaps'
 else
   -- ordinary Neovim
-  print 'hello from neovim'
   require 'custom.core'
   require 'custom.lazy'
 end
