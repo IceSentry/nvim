8485c560d14a35cbbcdb913156ebe474e1d4adb1
IceSentry <c.giguere42@gmail.com>
2024-10-28 13:26:58 -0400

    add oil

---
+++ lua/custom/plugins/oil.lua
@@ -0,0 +1,37 @@
+return {
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+  -- Optional dependencies
+  dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+  -- dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if prefer nvim-web-devicons
+  config = function()
+    local detail = false
+    require('oil').setup {
+      columns = {
+        'icons',
+        -- 'size',
+        -- 'mtime'
+      },
+      view_options = {
+        show_hidden = true,
+      },
+      keymaps = {
+        ['<C-s>'] = false,
+        ['gd'] = {
+          desc = 'Toggle file detail view',
+          callback = function()
+            detail = not detail
+            if detail then
+              require('oil').set_columns { 'icon', 'permissions', 'size', 'mtime' }
+            else
+              require('oil').set_columns { 'icon' }
+            end
+          end,
+        },
+      },
+    }
+    vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+  end,
+}
