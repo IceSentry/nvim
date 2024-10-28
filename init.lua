8ced37c8221efdc8e34c7f483973b29e80b3388e
IceSentry <c.giguere42@gmail.com>
2024-10-28 01:19:37 -0400

    use all of core in vscode

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 if vim.g.vscode then
   print 'Hello vscode'
   -- VSCode extension
-  require 'custom.core.keymaps'
+  require 'custom.core'
   require 'custom.plugins.mini'
   require 'custom.plugins.vim-visual-mutli'
 else
